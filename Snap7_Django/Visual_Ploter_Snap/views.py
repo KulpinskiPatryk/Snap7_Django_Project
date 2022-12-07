6ca42bee96a8d36746a43e031d3daafaae185696
Patryk_Kulpinski <61681867+KulpinskiPatryk@users.noreply.github.com>
2022-12-07 17:57:27 +0100

    Version 0.4

--- Snap7_Django/Visual_Ploter_Snap/views.py
+++ Snap7_Django/Visual_Ploter_Snap/views.py
@@ -1,6 +1,5 @@
 import json
 import time
-
 from django.shortcuts import redirect, render, HttpResponse, get_object_or_404
 from .models import *
 from django.shortcuts import get_object_or_404
@@ -14,6 +13,35 @@ import random as rand
 from datetime import datetime
 import threading
 import time
+import snap7
+
+IP = '192.168.100.100'
+RACK = 0
+SLOT = 1
+
+DB_NUMBER = 100
+START_ADRESS = 0
+SIZE = 7
+
+
+def read_from_PLC():
+    plc = snap7.client.Client()
+    plc.connect(IP, RACK, SLOT)
+
+    plc_info = plc.get_cpu_info()
+    print(f'Module Type: {plc_info.ModuleTypeName}')
+
+    state = plc.get_cpu_state()
+    print(f'State: {state}')
+
+    db = plc.db_read(DB_NUMBER, START_ADRESS, SIZE)
+    print(db)
+
+    temp = int.from_bytes(db[0:2], byteorder='big')
+    amp = int.from_bytes(db[2:4], byteorder='big')
+    volt = int.from_bytes(db[4:6], byteorder='big')
+
+    return temp, amp, volt
 
 
 def database_read(id_odczytu):
@@ -41,7 +69,7 @@ def index(request):
     odczyty_ids = list(odczyty_ids)
     print(liczba_odczytow)
     lod = liczba_odczytow.count()
-    nowy_odczyt(rand.randint(1, 100), rand.randint(1, 100), rand.randint(1, 100))
+    nowy_odczyt(read_from_PLC())
     if request.method == "POST":
         Odczyty.objects.all().delete()
         return redirect("/")
